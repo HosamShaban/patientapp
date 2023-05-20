ad3da6ff72130acba32f94cd2b76d77c844294a1
hosamshaban <97hosams@gmail.com>
2023-05-20 20:43:43 +0300

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_fire_cli/firebase_options.dart';
+import 'package:patientapp/firebase_options.dart';
 
-Future<void> main() {
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
