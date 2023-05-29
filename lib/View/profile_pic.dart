e65adaff2fb21d97f976863d49bbfe8def6774c4
hosamshaban <97hosams@gmail.com>
2023-05-29 19:03:25 +0300

    edit

--- lib/View/profile_pic.dart
+++ lib/View/profile_pic.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -13,24 +14,33 @@ class ProfilePic extends StatefulWidget {
 
 class _ProfilePicState extends State<ProfilePic> {
   XFile? _image;
-  _imgFromCamera()  {
-    XFile image =  ImagePicker.platform.pickImage(
-        source: ImageSource.camera, imageQuality: 50
-    ) as XFile;
 
-    setState(() {
-      _image = image;
-    });
+  _imgFromCamera() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile? image = await _picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+    );
+
+    if (image != null) {
+      setState(() {
+        _image = image;
+      });
+    }
   }
 
-  _imgFromGallery() {
-    File image = ImagePicker.platform.pickImage(
-        source: ImageSource.gallery, imageQuality: 50
-    ) as File;
+  _imgFromGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile? image = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+    );
 
-    setState(() {
-      _image = image as XFile?;
-    });
+    if (image != null) {
+      setState(() {
+        _image = image;
+      });
+    }
   }
 
   @override
@@ -42,29 +52,33 @@ class _ProfilePicState extends State<ProfilePic> {
         fit: StackFit.expand,
         clipBehavior: Clip.none,
         children: [
-          const CircleAvatar(
-            backgroundImage: AssetImage("assets/images/p.png"),
+          CircleAvatar(
+            backgroundImage: _image != null
+                ? FileImage(File(_image!.path)) as ImageProvider<Object>
+                : AssetImage("assets/images/p.png"),
           ),
           Positioned(
-              right: -16,
-              bottom: 0,
-              child: Container(
-                  decoration: BoxDecoration(
-                      // border: Border.all(),
-                      borderRadius: BorderRadius.circular(25.0),
-                      color: Colors.white),
-                  child: IconButton(
-                      color: Colors.black,
-                      onPressed: () async {
-                        final ImagePicker _picker = ImagePicker();
-                        final img = await _picker.pickImage(
-                            source: ImageSource.gallery);
-                        setState(() {
-                          _image = img;
-                        });
-                      },
-                      icon: const Icon(Icons.camera_alt,
-                          size: 22, color: Colors.black)))),
+            right: -16,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.white,
+              ),
+              child: IconButton(
+                color: Colors.black,
+                onPressed: () async {
+                  final ImagePicker _picker = ImagePicker();
+                  final img =
+                      await _picker.pickImage(source: ImageSource.gallery);
+                  setState(() {
+                    _image = img;
+                  });
+                },
+                icon: Icon(Icons.camera_alt, size: 22, color: Colors.black),
+              ),
+            ),
+          ),
         ],
       ),
     );
