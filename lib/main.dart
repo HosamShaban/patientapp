43825b5da9aaf89f27620b5d7936716ef5c7afa4
hosamshaban <97hosams@gmail.com>
2023-07-10 23:44:24 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:patientapp/controller/screenIndexProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:patientapp/View/personal_screen.dart';
-import 'package:patientapp/controller/screenIndexProvider.dart';
-import 'package:patientapp/onboarding.dart';
-import 'package:patientapp/widget/loading_animation_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'View/splash.dart';
+import 'controller/dio_helper.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DioHelper.init();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -17,21 +18,10 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  Future<bool> _checkEmailExists() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? email = prefs.getString('email');
-    return email != null;
-  }
-
-  Future<String?> _getToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('token');
-  }
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(392.72727272727275, 781.0909090909091),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
@@ -40,18 +30,7 @@ class MyApp extends StatelessWidget {
             ChangeNotifierProvider(create: (context) => screenIndexProvider())
           ],
           child: MaterialApp(
-            home: FutureBuilder<String?>(
-              future: _getToken(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return LoadingAnimationPage();
-                } else if (snapshot.hasData && snapshot.data != null) {
-                  return PersonalPage(); // Replace with your home screen widget
-                } else {
-                  return const OnBoarding();
-                }
-              },
-            ),
+            home: const SplashScreen(),
             debugShowCheckedModeBanner: false,
             theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Tajawal'),
           ),
