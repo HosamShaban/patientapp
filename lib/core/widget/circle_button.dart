95412d8687573e672f4850d8f147deb1392fd35a
hosamshaban <97hosams@gmail.com>
2023-07-22 21:20:25 +0300

    create CircleButton

--- lib/core/widget/circle_button.dart
+++ lib/core/widget/circle_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../Consts/colors.dart';
+import 'package:patientapp/core/resources/colors.dart';
 
 class CircleButton extends StatelessWidget {
   final bool isGreen;
@@ -13,12 +12,13 @@ class CircleButton extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(2),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-        ),
+        borderRadius: BorderRadius.circular(50),
+      ),
       child: Material(
         color: Colors.transparent,
         child: InkWell(
-          splashColor: isGreen ? ConstColors.primaryColor : ConstColors.backgroundColor,
+          splashColor:
+              isGreen ? ConstColors.primaryColor : ConstColors.backgroundColor,
           borderRadius: BorderRadius.circular(50),
           child: Icon(
             isAdd ? Icons.add : Icons.remove,
